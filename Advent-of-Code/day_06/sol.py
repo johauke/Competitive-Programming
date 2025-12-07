b932fba09f0c37b619c6ddd3b2a0e4e2f5f485e7
Jonas Haukenes <johauke06@gmail.com>
2025-12-07 16:46:23 +0100

    aoc day 6 & 7

---
+++ Advent-of-Code/day_06/sol.py
@@ -0,0 +1,62 @@
+from itertools import zip_longest
+f = open("input.txt")
+sample = f.read().splitlines()
+
+operations = sample[-1].split()
+sample.pop()
+
+def task_1():
+    table = []
+    for line in sample:
+        table.append(list(map(int, line.split())))
+
+    total = 0
+    for i, op in enumerate(operations):
+
+        if op == "*":
+            sum = 1
+            for r in range(len(table)):
+                sum *= table[r][i]
+            total += sum
+        else:
+            sum = 0
+            for r in range(len(table)):
+                sum += table[r][i]
+            total += sum
+
+    return total
+
+
+def task_2():
+    m = 0
+    sum = operations[m] == "*"
+    ll = [[] for _ in operations]
+
+    for i in range(len(sample[0])):
+        num = ""
+        for j in range(len(sample)):
+            num += sample[j][i]
+
+        num = num.strip()
+        if num == "":
+            m += 1
+            sum = operations[m] == "*"
+        else:
+            ll[m].append(int(num))
+
+    total = 0
+    for i, op in enumerate(operations):
+        sum = op == "*"
+        for num in ll[i]:
+            if op == "*":
+                sum *= num
+            else:
+                sum += num
+        total += sum
+
+    return total
+
+
+print(f"Solution task 1: {task_1()}")
+print(f"Solution task 2: {task_2()}")
+f.close()
---
+++ Advent-of-Code/day_07/sol.py
@@ -0,0 +1,46 @@
+from functools import cache
+f = open("input.txt")
+sample = f.read().splitlines()
+
+def find_start():
+    start = 0
+    for i, e in enumerate(sample[0]):
+        if e == "S":
+            start = i
+            break
+    return start
+
+def task_1():
+    count = 0
+    searching = {find_start()}
+    toSearch = set()
+    for line in sample[2::2]:
+        for s in searching:
+            if line[s] == "^":
+                toSearch.add(s-1)
+                toSearch.add(s+1)
+                count += 1
+            else:
+                toSearch.add(s)
+        searching = toSearch
+        toSearch = set()
+
+    return count
+
+def task_2():
+    @cache
+    def rec(row, col):
+        if row >= len(sample)-1:
+            return 1
+
+        if sample[row][col] == "^":
+            return rec(row+2, col-1) + rec(row+2, col+1)
+
+        return rec(row+2, col)
+
+    return rec(2, find_start())
+
+
+print(f"Solution task 1: {task_1()}")
+print(f"Solution task 2: {task_2()}")
+f.close()
